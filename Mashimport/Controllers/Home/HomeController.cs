466ebc2b8e06712329426677adf3dcbdde537930
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-13 22:26:25 +0300

    Первый контроллер

---
+++ Mashimport/Controllers/Home/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mashimport.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello world");
+        }
+    }
+}
--- Mashimport/Program.cs
+++ Mashimport/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
 
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.Run();
